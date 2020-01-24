e5ad0b35a24b79c71202b64487be3a3f5888810e
Diego Vasquez <botellongo69@outlook.com>
2020-01-24 18:25:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arrayName)
+  batch = []
+  arrayName.each do |i|
+    batch << badge_maker(i)
+  end
+  batch
+end
+
+def assign_rooms(arrayName)
+  batch = []
+  arrayName.each_with_index { |name, index|
+    batch << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  }
+  batch
+end
+
+def printer(attendees)
+  rooms.each do |i|
+    puts i
+  end
+end
